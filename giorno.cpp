d7575e885edd288458421acb94b93f5adcdafaa5
GabrieleCosta00 <gabriele.costa.1h@gmail.com>
2021-09-05 15:39:54 +0200

    Prima stesura del metodo di inserimento degli esami nel giorno.cpp

--- giorno.cpp
+++ giorno.cpp
@@ -3,12 +3,44 @@
 //
 
 #include <vector>
+#include <string>
 #include "exam.h"
+#include "slot.h"
 #include "giorno.h"
 
 
-//const vector <string> &giorno::get_id_corsi() const {
+bool giorno::set_id_esame_nel_giorno(const string &id_esame, const string &id_cds, const int anno, const int n_slot_necessari) {
 
-   // return esame;
+    bool esame_inserito;
+    int ins_nello_slot = 0;
 
-//}
+    while((ins_nello_slot + n_slot_necessari -1) < n_slot)
+    {
+        for(int i=0; i < n_slot_necessari; i++)
+        {
+            if (_fasce_orarie[i].set_id_esame_nello_slot(const string &id_esame, const vector<string> &id_professori,
+            int n_vers_paral))
+            {
+                esame_inserito = true;
+            }
+            else
+            {
+                esame_inserito = false;
+            }
+        }
+
+        if (esame_inserito)
+        {
+            _id_cds_inseriti.push_back(id_cds);
+            _anni_inseriti.push_back(anno);
+            return true;
+        }
+        else
+        {
+            ins_nello_slot++;
+        }
+    }
+
+    return false;
+
+}
